14a253a3bb53fff8196e09ed9b3190f6306a8e74
srivaa27 <aman15220@gmail.com>
2022-05-25 04:30:36 +0530

    inflating elements in MainActivity

--- app/src/main/java/com/example/addingmenu/MainActivity.java
+++ app/src/main/java/com/example/addingmenu/MainActivity.java
@@ -3,12 +3,40 @@ package com.example.addingmenu;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button mButton;
+    LinearLayout mLinearLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mButton = findViewById(R.id.button);
+        mLinearLayout = findViewById(R.id.linearLayout);
+
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                // getting inflater object
+                LayoutInflater inflater = getLayoutInflater();
+
+                // receiving our custom layout as view to add in our layout
+                View view1 = inflater.inflate(R.layout.new_layout,null);
+
+                mLinearLayout.setOrientation(LinearLayout.VERTICAL);
+                mLinearLayout.addView(view1);
+
+            }
+        });
+
+
+
+
     }
 }
